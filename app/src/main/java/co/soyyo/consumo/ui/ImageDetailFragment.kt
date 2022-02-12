eeb52129475f83610eb9f560f482b2b5a1add1cb
Chavez-Noe <noe_fam_chavez@yahoo.com>
2022-02-11 21:55:33 -0600

    finished view design.

---
+++ app/src/main/java/co/soyyo/consumo/ui/ImageDetailFragment.kt
@@ -0,0 +1,16 @@
+package co.soyyo.consumo.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.soyyo.consumo.R
+
+class ImageDetailFragment : Fragment(R.layout.fragment_image_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
